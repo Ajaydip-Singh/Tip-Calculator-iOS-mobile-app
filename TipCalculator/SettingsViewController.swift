117701ad6990471ba9c9e33729be98aeecdf2f86
Ajaydip Singh <ajaylottey@gmail.com>
2021-01-05 16:51:13 +0300

    :sparkles: feat: add view picker for selecting tips

--- TipCalculator/SettingsViewController.swift
+++ TipCalculator/SettingsViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
-    @IBOutlet weak var defaultTipTextField: UITextField!
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var defaultTipLabel: UILabel!
+    @IBOutlet weak var tipPicker: UIPickerView!
+    
+    let pickerData = ["15", "16", "17", "18", "19", "20"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tipPicker.delegate = self
+        tipPicker.dataSource = self
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.openTipSelector(_:)))
+                defaultTipLabel.addGestureRecognizer(tap)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,33 +42,61 @@ class SettingsViewController: UIViewController {
     
         let defaults = UserDefaults.standard
         let defaultTip = defaults.string(forKey: "tipPercentage") ?? "15"
-        defaultTipTextField.text = defaultTip
+        defaultTipLabel.text = defaultTip
+        let row = pickerData.firstIndex(of: defaultTip) ?? 0
+        tipPicker.selectRow(row, inComponent: 0, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
-    
-    
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    // Capture the picker view selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
         let defaults = UserDefaults.standard
-        defaults.setValue(defaultTipTextField.text, forKey: "tipPercentage")
+        
+        switch row {
+        case 0:
+            defaults.setValue("15", forKey: "tipPercentage")
+            defaultTipLabel.text = "15"
+        case 1:
+            defaults.setValue("16", forKey: "tipPercentage")
+            defaultTipLabel.text = "16"
+        case 2:
+            defaults.setValue("17", forKey: "tipPercentage")
+            defaultTipLabel.text = "17"
+        case 3:
+            defaults.setValue("18", forKey: "tipPercentage")
+            defaultTipLabel.text = "18"
+        case 4:
+            defaults.setValue("19", forKey: "tipPercentage")
+            defaultTipLabel.text = "19"
+        default:
+            defaults.setValue("20", forKey: "tipPercentage")
+            defaultTipLabel.text = "20"
+        }
         
     }
     
-
+    
+    @IBAction func openTipSelector(_ sender: Any) {
+        tipPicker.isHidden = false
+        doneButton.isHidden = false
+        
+    }
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
+        tipPicker.isHidden = true
+        doneButton.isHidden = true
+        
     }
+    
+    
+    @IBAction func onTipSelectionDone(_ sender: UIButton) {
+        tipPicker.isHidden = true
+        doneButton.isHidden = true
+    }
+    
+    
+    
+    
 }
